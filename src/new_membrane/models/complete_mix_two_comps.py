cc6ad09eecdd3e02549dab574df4602e0cd8d797
HumanityJ <jhumanb1@gmail.com>
2021-12-30 22:05:19 +0000

    get xf & xo

--- src/new_membrane/models/complete_mix_two_comps.py
+++ src/new_membrane/models/complete_mix_two_comps.py
@@ -1,12 +1,34 @@
+from new_membrane import components
 from new_membrane.components import StreamConstructor
 from new_membrane.components import MembraneConstructor
 
 
 class CompleteMixTwo:
+    """
+    Transport Processes and Separation Process Principles (Includes Unit Operations) Fourth Edition 13.4
+    """
+
     def __init__(self, dir, input_stream, output_stream, membrane) -> None:
-        self._input_stream = StreamConstructor(dir, input_stream)
-        self._output_stream = StreamConstructor(dir, output_stream)
-        self._membrane = MembraneConstructor(dir, membrane)
+        self._input_stream = StreamConstructor(dir, input_stream).stream
+        self._output_stream = StreamConstructor(dir, output_stream).stream
+        self._membrane = MembraneConstructor(dir, membrane).membrane
+        self._sys_vars = self._unpack_sys_vars()
+
+    def _unpack_sys_vars(self):
+        composition_in = list(self._input_stream.composition.values())
+        composition_out = list(self._input_stream.composition.values())
+        return {
+            "xf": (composition_in[0] if composition_in[0] else 0),
+            "xo": (composition_out[0] if composition_out[0] else 0),
+        }
+
+    @property
+    def xf(self):
+        return self._sys_vars["xf"]
+
+    @property
+    def xo(self):
+        return self._sys_vars["xo"]
 
 
 if __name__ == "__main__":
--- tests/test_models.py/test_complete_mix_two_comps.py
+++ tests/test_models.py/test_complete_mix_two_comps.py
@@ -1,5 +1,5 @@
 from new_membrane.models.complete_mix_two_comps import CompleteMixTwo
-from tests.conftest import set_up_membrane, set_up
+from conftest import set_up, set_up_membrane
 
 DIR_PATH = "C:\\Users\\jhuma\\OneDrive\Desktop\python\\new-membrane\\tests\\test_data"
 
@@ -9,7 +9,6 @@ class TestCompleteMixTwo:
     stream_file = "stream_data.json"
 
     def test_create_instance(self):
-        print("Hello")
         model = CompleteMixTwo(
             DIR_PATH,
             set_up(self.stream_file),
@@ -17,3 +16,34 @@ class TestCompleteMixTwo:
             set_up_membrane(self.membrane_file),
         )
         assert isinstance(model, CompleteMixTwo)
+
+
+class TestCompleteMixTwoGet:
+    membrane_file = "membrane_data.json"
+    stream_file = "stream_data.json"
+
+    stream_data_in = {
+        "composition": {"CO2": 0.5, "N2": 0.5},
+        "flow_rate": 100.0,
+        "temperature": 50.0,
+        "pressure": 80,
+    }
+    stream_data_out = {
+        "composition": {"CO2": 0.25, "N2": 0.75},
+        "flow_rate": 0,
+        "temperature": 50.0,
+        "pressure": 80,
+    }
+
+    model = CompleteMixTwo(
+        DIR_PATH,
+        set_up(stream_file, "data", stream_data_in),
+        set_up(stream_file, "data", stream_data_out),
+        set_up_membrane(membrane_file),
+    )
+
+    def test_get_xf(self):
+        assert self.model.xf == 0.5
+
+    def test_get_xo(self):
+        assert self.model.xo == 0.25
