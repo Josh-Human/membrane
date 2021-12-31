7422c2bb165374ba9f19cf7f435c6f693e2c936d
HumanityJ <jhumanb1@gmail.com>
2021-12-31 17:31:03 +0000

    document complete_two_mix_comps

--- src/new_membrane/models/complete_mix_two_comps.py
+++ src/new_membrane/models/complete_mix_two_comps.py
@@ -5,35 +5,53 @@ import math
 
 
 class CompleteMixTwo:
-    """
+    """Complete mix membrane with two components.
+
     Transport Processes and Separation Process Principles (Includes Unit Operations) Fourth Edition 13.4
     """
 
     def __init__(
-        self, dir, input_stream, output_stream, permeate_stream, membrane
+        self,
+        dir: str,
+        feed_stream: str,
+        reject_stream: str,
+        permeate_stream: str,
+        membrane: str,
     ) -> None:
-        self._input_stream = StreamConstructor(dir, input_stream).stream
-        self._output_stream = StreamConstructor(dir, output_stream).stream
+        """Given file names and directory for data to construct Streams & Membrane.
+
+        :param _feed_stream: Stream object representing the feed flow.
+        :param _reject_stream: Stream object representing the reject flow.
+        :param _permeate_stream: Stream object representing the permeate/product flow.
+        :param _membrane: Membrane object represnting the membrane.
+        :param _sys_vars: Dictionary of the 7 variables neccasary to model a system. xf, xo, yp, θ, alpha, pl/ph, and Am, four of which are independent variables. Full definiton may be found in the class docstring.
+        """
+        self._feed_stream = StreamConstructor(dir, feed_stream).stream
+        self._reject_stream = StreamConstructor(dir, reject_stream).stream
         self._permeate_stream = StreamConstructor(dir, permeate_stream).stream
         self._membrane = MembraneConstructor(dir, membrane).membrane
         self._sys_vars = self._unpack_sys_vars()
 
-    def _unpack_sys_vars(self):
+    def _unpack_sys_vars(self) -> dict:
+        """Unpacks and calculates system variables from Streams & Membrane.
 
-        xf = list(self._input_stream.composition.values())[0]
+        If values are None then sets to 0.
+        """
 
-        xo = list(self._output_stream.composition.values())[0]
+        xf = list(self._feed_stream.composition.values())[0]
+
+        xo = list(self._reject_stream.composition.values())[0]
 
         alpha = (
             list(self._membrane.permeability.values())[0]
             / list(self._membrane.permeability.values())[1]
         )
 
-        pr = self._permeate_stream.pressure / self._input_stream.pressure
+        pr = self._permeate_stream.pressure / self._feed_stream.pressure
 
         yp = list(self._permeate_stream.composition.values())[0]
 
-        cut = self._permeate_stream.flow / self._output_stream.flow
+        cut = self._permeate_stream.flow / self._reject_stream.flow
 
         area = self._membrane.area
         return {
@@ -47,38 +65,63 @@ class CompleteMixTwo:
         }
 
     @property
-    def xf(self):
+    def xf(self) -> float:
+        """
+        :getter: get feed fraction of component A
+        """
         return self._sys_vars["xf"]
 
     @property
-    def xo(self):
+    def xo(self) -> float:
+        """
+        :getter: get reject fraction of component A
+        """
         return self._sys_vars["xo"]
 
     @property
-    def alpha(self):
+    def alpha(self) -> float:
+        """
+        :getter: get ratio of permeabilities
+        """
         return self._sys_vars["alpha"]
 
     @property
-    def pr(self):
+    def pr(self) -> float:
+        """
+        :getter: get ratio of pressure
+        """
         return self._sys_vars["pr"]
 
     @property
-    def yp(self):
+    def yp(self) -> float:
+        """
+        :getter: get permeate fraction of component A
+        """
         return self._sys_vars["yp"]
 
     @property
-    def area(self):
+    def area(self) -> float:
+        """
+        :getter: get total membrane area
+        """
         return self._sys_vars["area"]
 
     @property
-    def cut(self):
+    def cut(self) -> float:
+        """
+        :getter: get ratio of permeate flow to feed flow
+        :setter: set cut
+        """
         return self._sys_vars["cut"]
 
     @cut.setter
-    def cut(self, value):
+    def cut(self, value) -> None:
         self._sys_vars["cut"] = value
 
-    def calculate_cut(self):
+    def calculate_cut(self) -> float:
+        """Find cut value given xf, xo, alpha and pl/ph.
+
+        Calculates yp using root of quadratic equation. This is then used to solve for cut. Details of equations used may be found in Class docstring."""
         a = 1 - self._sys_vars["alpha"]
 
         b = (
@@ -99,19 +142,10 @@ class CompleteMixTwo:
 
         return self._sys_vars["cut"]
 
-    def calculate_area(self):
-        self._sys_vars["area"] = (
-            self._sys_vars["cut"] * self._input_stream.flow * self._sys_vars["yp"]
-        ) / (
-            (list(self._membrane.permeability.values())[0] / self._membrane.thickness)
-            * (
-                self._input_stream.pressure * self._sys_vars["xo"]
-                - self._permeate_stream.pressure * self._sys_vars["yp"]
-            )
-        )
-        return self._sys_vars["area"]
+    def calculate_xo(self) -> float:
+        """Find cut value given xf, θ, alpha and pl/ph.
 
-    def calculate_xo(self):
+        Calculates yp using root of quadratic equation. This is then used to solve for xo. Details of equations used may be found in Class docstring."""
         a = (
             self._sys_vars["cut"]
             + self._sys_vars["pr"]
@@ -143,7 +177,27 @@ class CompleteMixTwo:
 
         return self._sys_vars["xo"]
 
-    def calculate_min_reject(self):
+    def calculate_area(self) -> float:
+        """Calculates total membrane area in cm^2.
+
+        May be called after calculate_xo() or calculate_cut().
+        """
+        self._sys_vars["area"] = (
+            self._sys_vars["cut"] * self._feed_stream.flow * self._sys_vars["yp"]
+        ) / (
+            (list(self._membrane.permeability.values())[0] / self._membrane.thickness)
+            * (
+                self._feed_stream.pressure * self._sys_vars["xo"]
+                - self._permeate_stream.pressure * self._sys_vars["yp"]
+            )
+        )
+        return self._sys_vars["area"]
+
+    def calculate_min_reject(self) -> float:
+        """Given xf, alpha and pl/ph calculates xo.
+
+        Calculates the minimum reject composition based on the feed flow and other system properties. Even with an infintely large membrane the reject can not go below this value for a given composition, however a cascade or plug flow system may be used.
+        """
         self._sys_vars["xo"] = (
             self._sys_vars["xf"]
             * (
