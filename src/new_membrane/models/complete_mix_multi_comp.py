327d67d9e7a4de4e3e7337b54e3629d9b99aa3e7
HumanityJ <jhumanb1@gmail.com>
2022-01-02 22:49:07 +0000

    add documenation to complete mix multi component

--- src/new_membrane/models/complete_mix_multi_comp.py
+++ src/new_membrane/models/complete_mix_multi_comp.py
@@ -4,9 +4,9 @@ import scipy.optimize
 
 
 class CompleteMix:
-    """Complete mix membrane with multiple components.
+    """Complete mixing model membrane with multiple components.
 
-    Transport Processes and Separation Process Principles (Includes Unit Operations) Fourth Edition 13.4
+    Transport Processes and Separation Process Principles (Includes Unit Operations) Fourth Edition 13.5
     """
 
     def __init__(
@@ -23,7 +23,7 @@ class CompleteMix:
         :param _reject_stream: Stream object representing the reject flow.
         :param _permeate_stream: Stream object representing the permeate/product flow.
         :param _membrane: Membrane object represnting the membrane.
-        :param _sys_vars: Dictionary of the 7 variables neccasary to model a system. xf, xo, yp, θ, alpha, pl/ph, and Am, four of which are independent variables. Full definiton may be found in the class docstring.
+        :param _sys_vars: Dictionary of the 7 variables neccasary to model a system. xf, xo, yp, θ, alpha, pl/ph, and Am, four of which are independent variables. Examples may be found in reference in class docstring.
         """
         self._feed_stream = StreamConstructor(dir, feed_stream).stream
         self._reject_stream = StreamConstructor(dir, reject_stream).stream
@@ -34,14 +34,24 @@ class CompleteMix:
 
     @property
     def feed_composition(self) -> None:
+        """
+        :getter: get feed stream composition
+        """
         return self._feed_stream.composition
 
     @property
     def feed_flow(self) -> float:
+        """
+        :getter: get feed stream total flowrate
+        """
         return self._feed_stream.flow
 
     @property
     def cut(self) -> float:
+        """
+        :getter: get cut of system
+        :setter: set cut of system
+        """
         return self._known_vars["cut"]
 
     @cut.setter
@@ -50,22 +60,37 @@ class CompleteMix:
 
     @property
     def pl(self) -> float:
+        """
+        :getter: get retenate pressure
+        """
         return self._feed_stream.pressure
 
     @property
     def ph(self) -> float:
+        """
+        :getter: get feed side pressure
+        """
         return self._permeate_stream.pressure
 
     @property
     def permeabilities(self) -> dict:
+        """
+        :getter: get permeability of components
+        """
         return self._membrane.permeability
 
     @property
     def thickness(self) -> float:
+        """
+        :getter: get membrane thickness in m
+        """
         return self._membrane.thickness
 
     @property
     def permeate_composition(self) -> dict:
+        """
+        :getter: get permeate composition
+        """
         return self._permeate_stream.composition
 
     def _unpack_known_vars(self) -> dict:
@@ -101,6 +126,10 @@ class CompleteMix:
         }
 
     def calculate_permeate_composition(self, initial_guess: float = None) -> float:
+        """Calculates permeate composition and returns a value for root finding.
+
+        Permeate composition of component A is used as an initial value for root finding, being either calculated or set. Return a value to be used in the newton method to find the final permeate composition.
+        """
         self._set_permeate_composition(initial_guess)
         self._calculate_permeate_flow()
         self._calculate_area()
@@ -110,6 +139,7 @@ class CompleteMix:
         return 1 - sum(self._permeate_stream.composition.values())
 
     def _set_permeate_composition(self, initial_guess: float = None) -> None:
+        """Sets mole fraction of component A."""
         if initial_guess:
             self._permeate_stream.composition = [initial_guess]
             self._unknown_vars["ypa"] = initial_guess
@@ -120,6 +150,7 @@ class CompleteMix:
             self._unknown_vars["ypa"] = self._known_vars["feed_comp"][0] + 0.01
 
     def _calculate_permeate_flow(self) -> None:
+        """Permeate flow is calculated using current composition guess."""
         self._permeate_stream.flow = (
             self._known_vars["cut"] * self._known_vars["feed_flow"]
         )
@@ -128,6 +159,7 @@ class CompleteMix:
         )
 
     def _calculate_area(self) -> None:
+        """Membrane area calculated using current iteration values."""
         self._membrane.area = (
             self._permeate_stream.flow
             * self._unknown_vars["ypa"]
@@ -145,6 +177,10 @@ class CompleteMix:
         )
 
     def _calculate_permeate_compositions(self) -> None:
+        """All permeate compositions calculated.
+
+        Although all permeate compositions are calculated, component A should not be. As a result after this function is called component A is reset  to the iteration guess value after.
+        """
         for k in self._permeate_stream.composition.keys():
             self._permeate_stream.composition[k] = (
                 self._known_vars["ph"]
@@ -161,7 +197,11 @@ class CompleteMix:
                 + self._known_vars["pl"]
             )
 
-    def calculate_reject_composition(self, yp0: float = None) -> None:
+    def calculate_reject_composition(self, yp0: float = None) -> dict:
+        """Calculate and return reject composition.
+
+        Finds final permeate composition, which is then used to calculate other system values and finally the reject composition.
+        """
         scipy.optimize.newton(self.calculate_permeate_composition, yp0)
 
         for k in self._reject_stream.composition.keys():
