60ab7f02ab98d93afbd3f752398d9eedb0f89d28
HumanityJ <jhumanb1@gmail.com>
2022-01-27 22:13:10 +0000

    add type validators

--- src/new_membrane/utils/utils.py
+++ src/new_membrane/utils/utils.py
@@ -1,5 +1,6 @@
 from typing import Union
 from numbers import Number as num
+from abc import ABC, abstractmethod
 
 
 def check_values_positive(obj: Union[list, dict]) -> bool:
@@ -26,3 +27,45 @@ def check_and_update(self, attr: str, newValues: Union[list, dict]) -> None:
         getattr(self, attr).update(zip(getattr(self, attr), newValues))
     else:
         getattr(self, attr).update(newValues)
+
+
+class Validator(ABC):
+    def __set_name__(self, owner, name):
+        self.private_name = "_" + name
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
+
+    def __set__(self, obj, value):
+        self.validate(value)
+        setattr(obj, self.private_name, value)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class Number(Validator):
+    def __init__(self, minvalue=None, maxvalue=None):
+        self.minvalue = minvalue
+        self.maxvalue = maxvalue
+
+    def validate(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"Expected {value!r} to be an int or float")
+        if self.minvalue is not None and value < self.minvalue:
+            raise ValueError(f"Expected {value!r} to be at least {self.minvalue!r}")
+        if self.maxvalue is not None and value > self.maxvalue:
+            raise ValueError(f"Expected {value!r} to be no more than {self.maxvalue!r}")
+
+
+class PositiveDictionary(Validator):
+    def __init__(self):
+        pass
+
+    def validate(self, value):
+        if not isinstance(value, dict):
+            raise TypeError(f"Expected {value!r} to be a dict.")
+
+        if not all(v >= 0 for v in iter(value.values())):
+            raise ValueError(f"Expected {value!r} values to be positive.")
